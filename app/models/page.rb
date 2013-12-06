c29a7abbb9d501bbcb02326d1235106f0cf32111
Nate <namoricoo@hotmail.com>
2013-12-05 23:58:50 -0800

    End of chapter 8

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,7 @@
 class Page < ActiveRecord::Base
+  #How to provide a foreign_key
+  #belongs_to :subject, {foreign_key: "subject_id"}
+   belongs_to :subject
+   has_many :sections
+  
 end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,5 +1,7 @@
 class Subject < ActiveRecord::Base
+  has_many :pages
+  
   scope :visible, where(visible: true)
   scope :invisible, where(visible: false)
-  scope :search, lamda {|query| where(["name LIKE ?", "%#{query}%"])}
+  #scope :search, lamda {|query| where(["name LIKE ?", "%#{query}%"])}
 end
