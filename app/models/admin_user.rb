c2d4825038293d1a80b2f7cc3a030035786e0b7b
Nate <namoricoo@hotmail.com>
2013-12-05 18:57:26 -0800

    Finish chapter 7

---
+++ app/models/admin_user.rb
@@ -0,0 +1,4 @@
+class AdminUser < ActiveRecord::Base
+  #To configure a different table name
+  #set_table_name("other_table_name")
+end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,5 @@
 class Subject < ActiveRecord::Base
+  scope :visible, where(visible: true)
+  scope :invisible, where(visible: false)
+  scope :search, lamda {|query| where(["name LIKE ?", "%#{query}%"])}
 end
